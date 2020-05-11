336ed043d4a993f33abc0f2d2725f08cefbdfda4
Rafael Seixista <63550069+rafaelseixista@users.noreply.github.com>
2020-05-11 15:50:02 -0300

    conclusão do site que muda de cor com a mudança das horas
    site teste html concluido

--- estudo JS/estudo JS 1/sites Curso em Video/siteteste.js
+++ estudo JS/estudo JS 1/sites Curso em Video/siteteste.js
@@ -1,1 +1,21 @@
-alert('Olá Amigos!')
+function carregar() {
+    var msg= document.getElementById('msg')
+var img= document.getElementById('imagem')
+var data= new Date()
+var hora= data.getHours()
+msg.innerHTML=`Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // Bom Dia!
+    img.src = "manha_Easy-Resize.com_1.png"
+    document.body.style.background = "#eaa758"
+}
+else if (hora >= 12 && hora <= 18) {
+    //Boa Tarde
+    img.src = "tardinha_Easy-Resize.com_1.png"
+    document.body.style.background = "#fe624c"
+} else {
+    //Boa noite
+    img.src = "fotonoite_Easy-Resize.com.png"
+    document.body.style.background = "#016076"
+}
+}
